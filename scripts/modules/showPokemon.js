0c377fb8e2e6fe591f22ae5126486b1b1817bb65
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-09-16 08:47:00 -0300

    duvidas e mais duvidas

--- scripts/modules/showPokemon.js
+++ scripts/modules/showPokemon.js
@@ -8,19 +8,22 @@ export default function exibirPokemon() {
 
         pokeAPI(`pokemon/${selectValue}`)
             .then(response => {
-                // console.log(response)
+                console.log(response.data.id)
                 return response.data.id;
             })
             .then(id => pokeAPI(`evolution-chain/${id}/`))
-            .then(response => response.data.chain.evolves_to)
+            .then(response => response.data.chain.species)
             .then(evolutions => {
-                const evolution2 = evolutions[0].species.name;
-                document.querySelector('.pokemon-result').innerHTML = evolution2
+                // const evolution2 = evolutions[0].species;
+                // document.querySelector('.pokemon-result').innerHTML = evolution2;
+                console.log(evolutions)
                 
-                console.log(evolutions[0].evolves_to[0])
+                // console.log(evolution2)
+                // console.log(evolutions[0].evolves_to[0])
             })
             // adicionar catch
             
+            // ------------------------     POR QUE ELE TA BUSCANDO UM ID DIFERENTE DA SUA EVOLUÇÃO??????????????????????????????????????  -----------------------------------------
     })
     } catch (error) {
         console.error(error);
